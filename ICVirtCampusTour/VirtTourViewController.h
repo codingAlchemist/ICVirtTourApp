22b95343e052fe67d1ef5850be1ddbb5eed53bcb
codingAlchemist <tengence@gmail.com>
2013-03-27 19:09:21 -0400

    Initial Commit

---
+++ ICVirtCampusTour/VirtTourViewController.h
@@ -0,0 +1,13 @@
+//
+//  VirtTourViewController.h
+//  ICVirtCampusTour
+//
+//  Created by jason debottis on 3/27/13.
+//  Copyright (c) 2013 IC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VirtTourViewController : UIViewController
+
+@end
---
+++ ICVirtCampusTour/VirtTourViewController.m
@@ -0,0 +1,29 @@
+//
+//  VirtTourViewController.m
+//  ICVirtCampusTour
+//
+//  Created by jason debottis on 3/27/13.
+//  Copyright (c) 2013 IC. All rights reserved.
+//
+
+#import "VirtTourViewController.h"
+
+@interface VirtTourViewController ()
+
+@end
+
+@implementation VirtTourViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
